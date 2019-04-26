29a7f4437e3dc2f0103d2863b90cb47f95a6ca33
dar.serebr <darkdern@gmail.com>
2019-04-26 18:56:46 +0300

    chapter02

---
+++ src/main/java/com/daria/javacore/chapter02/Example2.java
@@ -0,0 +1,12 @@
+package com.daria.javacore.chapter02;
+
+class Example2 {
+    public static void main(String args[]) {
+        int num;
+        num = 100;
+        System.out.println("Это пременная num: " + num);
+        num = num * 2;
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
---
+++ src/main/java/com/daria/javacore/chapter02/ifSample.java
@@ -0,0 +1,30 @@
+package com.daria.javacore.chapter02;
+
+public class ifSample {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) {
+            System.out.println("x меньше y");
+        }
+
+        x = x * 2;
+
+        if (x == y) {
+            System.out.println("x теперь равно y");
+        }
+
+        x = x * 2;
+
+        if (x > y) {
+            System.out.println("x теперь больше y");
+        }
+
+        if (x == y) {
+            System.out.println("вы не увидите этого");
+        }
+    }
+}
